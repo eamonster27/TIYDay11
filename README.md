9b9b3ef2dc4af5487822105d9464acfa1ef809bf
EamonBachari <eamonbachari@gmail.com>
2017-06-06 10:39:30 -0500

    js101 first six questions complete

---
+++ README.md
@@ -0,0 +1,1 @@
+Projects and activities done on day 11 of The Iron Yard coding bootcamp. Emphasis on Javascript.
--- js101-master/tmp.js
+++ js101-master/tmp.js
@@ -2,25 +2,25 @@ var assert = require('chai').assert
 
 // Example
 
-// The purpose of this first exercise is just to show how 
-// assertions work and how the homework should be carried out. 
+// The purpose of this first exercise is just to show how
+// assertions work and how the homework should be carried out.
 // We'll do it together in class.
 
 // Part EXAMPLE
 
-// Write a function called addOne() that returns the input number, plus one. 
+// Write a function called addOne() that returns the input number, plus one.
 
 var addOne = function(num) {
-	return num + 1
-}
+	return num + 1;
+};
 
 // Part 1
 
-// Fix the following code so doubleNum(2) wil equal 4. 
+// Fix the following code so doubleNum(2) wil equal 4.
 
 var doubleNum = function(num) {
-	num * 2
-}
+	return num * 2;
+};
 
 /**
  * PART 2
@@ -28,7 +28,12 @@ var doubleNum = function(num) {
  * Write a function called sum that takes two numbers as
  * inputs and computes the sum of those two numbers.
  */
-
+ function sum(num0, num1){
+ 	if(((typeof num0) !== "number" || (typeof num1) !== "number") || (isNegative(num0) || isNegative(num1))) {
+ 		return null;
+ 	}
+  return num0 + num1;
+ }
  // PART 2.5
 
 // modify your sum() function so that it will return null
@@ -38,42 +43,67 @@ var doubleNum = function(num) {
 
 // Write a function called average that takes two numbers
 // as inputs and returns the average of the two. To avoid
-// repeating yourself, use your sum function inside 
+// repeating yourself, use your sum function inside
 // of your average function.
-
+function average(num0, num1){
+	return sum(num0, num1) / 2;
+}
 
 // Part 4
 
-// Write a function called isNegative that will tell 
+// Write a function called isNegative that will tell
 // whether a number is negative or not.
+function isNegative(num){
+	return num < 0;
+}
 
-
-// Now, modify your sum function again, so that it 
+// Now, modify your sum function again, so that it
 // will return null if any of the inputs is negative.
-// To avoid repeating yourself, use your isNegative 
+// To avoid repeating yourself, use your isNegative
 // function inside your sum funciton ***
 
 
 // Part 5
 
-// Write a function that will find the minimum of four 
+// Write a function that will find the minimum of four
 // input numbers. You can do it using nested if statements,
 // boolean operators, or both (but not neither).
-
+function minFunc(num0, num1, num2, num3) {
+	var min = num0;
+	if(min > num1) {
+		min = num1;
+	}
+	else if(min > num2) {
+		min = num2;
+	}
+	else if(min > num3) {
+		min = num3;
+	}
+	return min;
+}
 
 // Part 6
 
 // Using logical operators, write a function that will
-// return true if either input is a string, but not 
-// both or neither. 
+// return true if either input is a string, but not
+// both or neither.
 
+function isString(arg0, arg1) {
+	if(typeof(arg0) === "string" && typeof(arg1) !== "string"){
+		return true;
+	}
+	else if(typeof(arg0) !== "string" && typeof(arg1) === "string"){
+		return true;
+	}
+	return false; 
+}
 
 // HARD MODE
 
 // Part 7
 
-// Write a function called doTwice that takes as input a 
-// function and invokes that function twice. 
+// Write a function called doTwice that takes as input a
+// function and invokes that function twice.
 
 // For testing purposes, I've included some functions that
 // modify global variables, although that's not a good
@@ -88,14 +118,14 @@ var helloWorld = function() {
 }
 
 
-doTwice(helloWorld) // should print 'hello world' to the console twice. 
+doTwice(helloWorld) // should print 'hello world' to the console twice.
 
 
 // Part 8
 
 // Write a function called conditionallyInvoke that takes a function
-// as input. It should invoke that input function only if 
-// the value of a certain global variable, called ORACLE, is 
+// as input. It should invoke that input function only if
+// the value of a certain global variable, called ORACLE, is
 // "YES." Otherwise, it will does nothing.
 
 var conditionallyInvoke = function() {
@@ -127,13 +157,12 @@ var factory = function() {
 var factory2 = function() {
 	// you want more?
 }
-	
+
 // Part 11
 
 var factory3 = function(stuff) {
-	// This can't be healthy...	
+	// This can't be healthy...
 }
-
 console.log('********************************************************************************************************************************************')
 console.log('')
 console.log('Test Results Below')
